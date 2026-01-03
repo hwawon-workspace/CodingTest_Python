977e869713b76cd6f8000f49a3beafa597293b59
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-01-03 13:55:30 +0900

    add another solution

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -7,3 +7,7 @@ def solution(array, commands):
         tmp.sort()
         answer.append(tmp[k])
     return answer
+
+# 다른 사람 풀이: lambda 사용
+def solution(array, commands):
+    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))

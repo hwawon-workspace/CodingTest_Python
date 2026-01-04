ebdeca20d265fc4a4eaf9f8d4327b3c22fe5ed2b
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-01-04 20:53:23 +0900

    Add shorter code

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.py
@@ -4,3 +4,7 @@ def solution(sizes):
     w, h = zip(*sizes)
     answer = max(w) * max(h)
     return answer
+
+# 다른 사람 풀이(같은 의미, 짧은 코드)
+def solution(sizes):
+    return max(max(size) for size in sizes) * max(min(size) for size in sizes)

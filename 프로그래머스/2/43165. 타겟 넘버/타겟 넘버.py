14981480ca859b1e79f9978b35104a58610967c7
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-01-09 00:00:53 +0900

    add another solution
    Added a new solution using recursion and itertools for the target number problem.

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,3 +1,4 @@
+# recursion 풀이
 def solution(numbers, target):
     def dfs(cnt, current_sum):
         if cnt == len(numbers):
@@ -9,3 +10,11 @@ def solution(numbers, target):
         return dfs(cnt + 1, current_sum + numbers[cnt]) + dfs(cnt + 1, current_sum - numbers[cnt])
 
     return dfs(0, 0)
+
+# map, product 활용 풀이
+from itertools import product
+
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)

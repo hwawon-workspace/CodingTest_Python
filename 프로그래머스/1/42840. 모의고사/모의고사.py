0c3926615699eadcac3bb696b18f1eaf725a1706
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-01-13 15:41:16 +0900

    Add shorter code
    Refactor solution function for clarity and efficiency.

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# 1. 내 풀이
+# 문제점: 불필요한 array 변환, 간결하지 않은 코드
 def solution(answers):
     score = [0, 0, 0, 0]
     st1 = [1,2,3,4,5]
@@ -22,3 +24,25 @@ def solution(answers):
     indexes = np.where(np.array(score) == max(np.array(score)))[0].tolist()
     
     return indexes
+
+# 2. 간결한 코드
+def solution(answers):
+    score =  [0, 0, 0]
+    st1 = [1,2,3,4,5]
+    st2 = [2,1,2,3,2,4,2,5]
+    st3 = [3,3,1,1,2,2,4,4,5,5]
+    answer = []
+
+    for idx, ans in enumerate(answers):
+        if ans == st1[idx % len(st1)]:
+            score[0] += 1
+        if ans == st2[idx % len(st2)]:
+            score[1] += 1
+        if ans == st3[idx % len(st3)]:
+            score[2] += 1
+
+    for idx, s in enumerate(score):
+        if s == max(score):
+            answer.append(idx + 1)
+
+    return answer

060c0686fdbe2dfa4681cf904876fc05756d54e1
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-04-30 20:09:09 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.12 ms, Memory: 38.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,12 +1,4 @@
 from collections import Counter
-# def solution(participant, completion):
-#     notcom = participant.copy()
-#     for c in completion:
-#         if c in notcom:
-#             notcom.remove(c)
-#     answer = notcom.pop()
-#     return answer
 
 def solution(participant, completion):
-    answer = list((Counter(participant) - Counter(completion)).keys())[0]
-    return answer
+    return list((Counter(participant) - Counter(completion)).keys())[0]

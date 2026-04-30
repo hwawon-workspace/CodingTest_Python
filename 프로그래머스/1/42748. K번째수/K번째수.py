c46025d67a1ef0b4ebccbc0881f225b0e1ccf68b
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-04-30 19:37:59 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.34 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,13 +1,3 @@
 def solution(array, commands):
-    answer = []
-    for c in commands:
-        i, j, k = c[0]-1, c[1]-1, c[2]-1
-        print(i,j,k)
-        tmp = array[i:j+1]
-        tmp.sort()
-        answer.append(tmp[k])
+    answer = [sorted(array[i-1:j])[k-1] for i,j,k in commands]
     return answer
-
-# 다른 사람 풀이: lambda 사용
-def solution(array, commands):
-    return list(map(lambda x:sorted(array[x[0]-1:x[1]])[x[2]-1], commands))

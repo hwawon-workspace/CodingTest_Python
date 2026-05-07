bfb47102f39c6a3fdfb9dc8b8ec04730cb178e73
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-05-07 17:13:57 +0900

    [level 3] Title: 입국심사, Time: 298.28 ms, Memory: 13.1 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,23 +1,22 @@
 def solution(n, times):
-    left = 1 # 최소 시간
-    right = max(times) * n # 최대 시간
+    left, right = 1, max(times)*n
     
-    answer = right # 최대로 가정
+    answer = right 
     
     while left <= right:
-        mid = (left + right) // 2
-        
-        people = 0
+        mid = (left+right) // 2 # 얼마나 걸릴지 시간을 이분법으로 구하는 것
+
+        people = 0 
         for t in times:
-            people += mid // t
+            people += mid // t # mid시간동안 t에서 처리할 수 있는 사람 수
             if people >= n:
                 break
-                
-        if people >= n:
-            answer = mid
-            right = mid - 1
-
-        else:
-            left = mid + 1
+        
+        if people >= n: # mid 안에 n명 충분히 가능
+            answer = mid # 정답 후보
+            right = mid - 1 # 최대값을 mid보다 작은 값으로 줄여서 다시 이분법
+            
+        else: # mid 안에 n명 불가능
+            left = mid + 1 # 최소를 늘려보기
             
     return answer

a7a7136308cfcfd7931657dfdbc5ebcef4461e16
SONG HWAWON <122874750+hwawon-workspace@users.noreply.github.com>
2026-05-21 22:57:38 +0900

    [level 3] Title: 거스름돈, Time: 354.07 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.py
@@ -0,0 +1,14 @@
+# 전체 경우의 수 구해야 함 -> Bottom-up, for문
+def solution(n, money):
+    MOD = 1000000007
+    
+    dp = [0] * (n+1) # 0원~ n원까지 만들 수 있는 경우의 수
+
+    # 현재 있는 동전으로 만들 수 있는 모든 금액의 경우의 수
+    dp[0] = 1
+    for m in money:
+        for price in range(m, n+1):
+            dp[price] += dp[price-m]
+            dp[price] % MOD
+    
+    return dp[n]
